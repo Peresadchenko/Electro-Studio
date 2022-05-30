ad06ed2f1335318becc53abef35d8290303a99cd
Peresadchenko <1pers1@bk.ru>
2022-05-30 18:30:13 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(function () {
 
-    //MENU-------------------------
+    // _____________MENU________________
 
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active');
         $('.menu').toggleClass('menu--open');
     });
 
+    // _____________gallery-slider________________
 
     $('.gallery-slider__inner').slick({
         responsive: [
@@ -61,6 +62,8 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+    // _____________reviews-slider________________
 
     $('.reviews-slider__inner').slick({
         responsive: [
@@ -117,6 +120,8 @@ $(function () {
         waitForAnimate: false
     });
 
+    // _____________info-slider________________
+
     $('.info-slider__inner').slick({
         responsive: [
             {
@@ -172,8 +177,67 @@ $(function () {
         waitForAnimate: false
     });
 
+    // _____________input tel________________
+
     $("input[type='tel']").click(function () {
         $(this).setCursorPosition(0);
     }).mask("+9 (999)-999-99-99");
 
+    // _____________timer________________
+
+    localStorage.getItem('timer')
+
+    let time = 600;
+    let timerTextOff = "Время истекло!";
+
+    let timer = document.getElementById('timer');
+
+    setInterval(сountDown, 1000);
+
+    function сountDown() {
+        let minutes = Math.floor(time / 60);
+        let hours = Math.floor(minutes / 60);
+        minutes = minutes - hours * 60;
+        let seconds = time % 60;
+
+        if (hours < 10)
+            hours = "0" + hours;
+
+        if (seconds < 10)
+            seconds = "0" + seconds;
+
+        if (minutes < 10)
+            minutes = "0" + minutes;
+
+        timer.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+        localStorage.setItem('timer', timer)
+
+        if (time === 0) {
+            document.getElementById('timerTextOff').innerHTML = timerTextOff;
+            return;
+        }
+
+        if (time > 0)
+            time--;
+
+        if (time < 0)
+            time = 0;
+
+    }
+
+    $(".footer__btn").on("click", function (e) {
+        e.preventDefault();
+        var id = $(this).attr("href"),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+    $(".menu a").on("click", function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
 });
+
